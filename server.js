1de87d5b744ccfe4c4c4c2fe522dd29056250509
Zweebl <sn4kez@googlemail.com>
2014-05-11 11:39:11 +0200

    Layout master added, scripts, css etc added with bower

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
-var express = require('express');
+var express = require('express'),
+    bodyParser =require('body-parser'),
+    morgan = require('morgan'),
+    stylus = require('stylus');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+function compile(src, path) {
+    return stylus(src).set('filename', path);
+}
+
 if('development' === env) {
     app.set('views', __dirname + '/server/views');
     app.set('view engine', 'jade');
+
+    app.use(morgan());
+    app.use(bodyParser());
+
+    app.use(stylus.middleware(
+        {
+            src: __dirname + '/public',
+            compile: compile
+        }
+    ));
+
+    app.use(express.static(__dirname + '/public'));
 }
 
 //app.configure(function() {

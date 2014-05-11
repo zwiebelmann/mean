9918d985a0c255546893e1949e687098fb0f5276
Zweebl <sn4kez@googlemail.com>
2014-05-11 10:48:05 +0200

    Server adjusted

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-app.configure(function() {
+if('development' === env) {
     app.set('views', __dirname + '/server/views');
     app.set('view engine', 'jade');
-});
+}
+
+//app.configure(function() {
+//    app.set('views', __dirname + '/server/views');
+//    app.set('view engine', 'jade');
+//});
 
 app.get('*', function(req, res) {
     res.render('index');
 });
+var port = 3030;
+app.listen(port);
+
+console.log('Listening on port ' + port + '...');

5bcbc8cea3c02ac617c9bf547ba852cdf173009b
Zweebl <sn4kez@googlemail.com>
2014-05-13 00:07:52 +0200

    Layout master added, scripts, css etc added with bower

---
+++ public/app/app.js
@@ -0,0 +1,9 @@
+angular.module('app', ['ngResource', 'ngRoute']);
+
+angular.module('app').config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+        .when('/', { templateUrl: '/partials/main', controller: 'mainCtrl'});
+});
+
+angular.module();
